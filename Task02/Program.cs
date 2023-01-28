f895f90f93adb25d2ba99e4b16c7d99f21d9eee5
Pavel Belov <job.pb@yandex.ru>
2023-01-28 18:41:52 +0300

    add home work 5 task 02

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать числа Фибоначчи
+
+Console.Clear();
+
+Console.Write("Введите количество чисел Фибоначчи: ");
+int count = int.Parse(Console.ReadLine()!);
+
+int[] arrayFib = new int[count];
+arrayFib[0] = 0;
+arrayFib[1] = 1;
+
+for (int i = 2; i <= arrayFib.Length - 1; i++)
+{
+    arrayFib[i] = arrayFib[i - 1] + arrayFib[i - 2];
+}
+
+Console.WriteLine($"Числа Фибоначчи: {String.Join(" ", arrayFib)}");
